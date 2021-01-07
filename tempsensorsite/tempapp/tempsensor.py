0f90cf7e43481029f3841d420e2417b4eacfa09c
tomosev <tomosev@pm.me>
2021-01-07 22:21:22 +0000

    test

--- tempsensorsite/tempapp/tempsensor.py
+++ tempsensorsite/tempapp/tempsensor.py
@@ -1,17 +1,5 @@
-import os
-
-global temperature2
-
-
-def sensor():
-    for i in os.listdir("/sys/bus/w1/devices"):
-        if i != "w1_bus_master1":
-            ds18b20 = i
-    return ds18b20
-
-
-def read(ds18b20):
-    location = "/sys/bus/w1/devices/" + ds18b20 + "/w1_slave"
+def read():
+    location = "/sys/bus/w1/devices/28-3c01d60784f5/w1_slave"
     tfile = open(location)
     text = tfile.read()
     tfile.close()
@@ -21,22 +9,3 @@ def read(ds18b20):
     celsius = temperature / 1000
     farenheit = (celsius * 1.8) + 32
     return celsius, farenheit
-
-
-def loop(ds18b20):
-    while True:
-        if read(ds18b20) != None:
-            temperature2 = "%0.3f C" % read(ds18b20)[0]
-            return temperature2
-
-
-def kill():
-    quit()
-
-
-if __name__ == "__main__":
-    try:
-        serialNum = sensor()
-        loop(serialNum)
-    except KeyboardInterrupt:
-        kill()
--- tempsensorsite/tempapp/views.py
+++ tempsensorsite/tempapp/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from tempapp import tempsensor
 
-temp = tempsensor.loop(ds18b20)
+
+def read():
+    location = "/sys/bus/w1/devices/28-3c01d60784f5/w1_slave"
+    tfile = open(location)
+    text = tfile.read()
+    tfile.close()
+    secondline = text.split("\n")[1]
+    temperaturedata = secondline.split(" ")[9]
+    temperature = float(temperaturedata[2:])
+    celsius = temperature / 1000
+    farenheit = (celsius * 1.8) + 32
+    return celsius, farenheit
+
+
+celsius = read().celsius
+farenheit = read().farenheit
 
 
 def temp(request):
-    context = {"temp": temp}
+    context = {"temp": celsius}
     return render(request, "tempapp/main.html", context)
