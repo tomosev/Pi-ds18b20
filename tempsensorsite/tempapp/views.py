1c14544ff0790b023562f99f504717d9455e7b86
tomosev <tomosev@pm.me>
2021-01-07 23:32:18 +0000

    12345

--- tempsensorsite/tempapp/views.py
+++ tempsensorsite/tempapp/views.py
@@ -2,15 +2,20 @@ from django.shortcuts import render
 
 
 def temp(request):
+    # The same as commenting out code but just to make it easier
+    testing = 0
     # This is the location of your device, the 28-3c01d60784f5 will be diffirent for everyone.
-    location = "/sys/bus/w1/devices/28-3c01d60784f5/w1_slave"
-    tfile = open(location)
-    text = tfile.read()
-    tfile.close()
-    secondline = text.split("\n")[1]
-    temperaturedata = secondline.split(" ")[9]
-    temperature = float(temperaturedata[2:])
-    celsius = temperature / 1000
-    farenheit = (celsius * 1.8) + 32
+    if testing == 1:
+        location = "/sys/bus/w1/devices/28-3c01d60784f5/w1_slave"
+        tfile = open(location)
+        text = tfile.read()
+        tfile.close()
+        secondline = text.split("\n")[1]
+        temperaturedata = secondline.split(" ")[9]
+        temperature = float(temperaturedata[2:])
+        celsius = temperature / 1000
+        farenheit = (celsius * 1.8) + 32
+    else:
+        celsius = 1
     context = {"temp": celsius}
     return render(request, "tempapp/main.html", context)
